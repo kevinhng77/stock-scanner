ebb2e993c4d641ac122052e9d9d17aac5b85e5ec
kevinhng77 <62964063+kevinhng77@users.noreply.github.com>
2023-04-11 22:51:47 -0700

    added call function

--- scanner.py
+++ scanner.py
@@ -1,4 +1,6 @@
 import json
+import os
+from twilio.rest import Client
 import requests
 from bs4 import BeautifulSoup
 import datetime, time
@@ -8,12 +10,24 @@ url= 'https://www.investing.com/equities/pre-market'
 now = datetime.now()
 timeCompare = now.replace(hour=5, minute=30)
 
-# if now > timeCompare:
-#     print("time is greater than 5:45")
+def callNow():
+    if now > timeCompare:
+        account_sid = "AC0dcd3d24de7ea7b04a8174dc18e22179"
+        auth_token = "31ada64acedb8e0211fd9f9e2c5ffed6"
+        client = Client(account_sid, auth_token)
+
+        call = client.calls.create(
+        url="http://demo.twilio.com/docs/voice.xml",
+        to="+14152603133",
+        from_="+18449292123"
+        )
+
+        print(call.sid)
+
+# r = requests.get(url)
+# soup = BeautifulSoup(r.content, 'html5lib')
+# mydivs = soup.find_all("div", {'class': 'PreMarketTopGainersLosersTable_wrapper__NWnQ0'} )
+# print(mydivs)
 
 
-
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html5lib')
-mydivs = soup.find_all("div", {'class': 'datatable_row__qHMpQ'} )
-print(mydivs)
+callNow()
